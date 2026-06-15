d06fb78c8ad93e9466df31bc3a657e0a214884f6
actionchain <actionchain@chainguard.dev>
2026-06-15 00:28:02 +0000

    Update WyriHaximus/github-action-get-previous-tag@v2.0.0

---
+++ main.js
@@ -0,0 +1,40 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+const tagPattern = `${process.env.INPUT_PATTERN || '*'}`;
+const workingDirectory = process.env.INPUT_WORKINGDIRECTORY || null;
+
+console.log('\x1b[33m%s\x1b[0m', 'Working directory: ', workingDirectory || '');
+
+exec(`git for-each-ref --sort=-refname --sort=-creatordate --count 1 --format="%(refname:short)" "refs/tags/${tagPattern}"`, {cwd: workingDirectory}, (err, tag, stderr) => {
+    tag = tag.trim();
+
+    if (err) {
+        console.log('\x1b[33m%s\x1b[0m', 'Could not find any tags because: ');
+        console.log('\x1b[31m%s\x1b[0m', stderr);
+        process.exit(1);
+    } else if (tag === "") {
+        let timestamp = Math.floor(new Date().getTime() / 1000);
+        console.log('\x1b[33m%s\x1b[0m', 'Falling back to default tag');
+        console.log('\x1b[32m%s\x1b[0m', `Found tag: ${process.env.INPUT_FALLBACK}`);
+        console.log('\x1b[32m%s\x1b[0m', `Found timestamp: ${timestamp}`);
+        fs.appendFileSync(process.env.GITHUB_OUTPUT, `tag=${process.env.INPUT_FALLBACK}\n`);
+        fs.appendFileSync(process.env.GITHUB_OUTPUT, `timestamp=${timestamp}\n`);
+        process.exit(0);
+    }
+
+    exec(`git log -1 --format=%at ${tag}`, {cwd: workingDirectory}, (err, timestamp, stderr) => {
+        if (err) {
+            console.log('\x1b[33m%s\x1b[0m', 'Could not find any timestamp because: ');
+            console.log('\x1b[31m%s\x1b[0m', stderr);
+            process.exit(1);
+        }
+
+        timestamp = timestamp.trim();
+
+        console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
+        console.log('\x1b[32m%s\x1b[0m', `Found timestamp: ${timestamp}`);
+        fs.appendFileSync(process.env.GITHUB_OUTPUT, `tag=${tag}\n`);
+        fs.appendFileSync(process.env.GITHUB_OUTPUT, `timestamp=${timestamp}\n`);
+        process.exit(0);
+    });
+});
